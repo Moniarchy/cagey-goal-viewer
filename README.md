7b80eb2d97a68eae03c37645749795215d4bf2a5
Devon Wesley <dwesley89@gmail.com>
2016-07-21 13:51:29 -0700

    init backend commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# cagey-lapwing
+lg Goal Viewer App
+
+## Description
+
+A repo that pulls in our goals/issues and displays them while maximizing a great UX.
+Basically, you need to duplicate the functionality of a Rails app: [GitHub repo] (https://github.com/bluemihai/goals-viewer), [live on Heroku](https://goals-viewer-api.herokuapp.com/).
+
+## Context
+
+Serving an actual customer is a huge part of writing good software.  If you take on this goal, you will be the customer, and have access to 19 other users - so you can talk to them, and learn about good User Experience Design (UX).
+
+Notes:
+build interface
+pull info from github, change data.. maybe no need to store data locally
+Create something useful, do user research on feature, integrate feedback into your system
+
+## Required Specs (Must Have)
+
+- [ ] You have interviewed at least three potential users at your app and integrated their feedback
+- [ ] Auth is handled through GitHub Oauth, site uses the GitHub API to access goals data from this repo's issues
+- [ ] An external, published, all-JS site which improves the UX of browsing/searching these goals
+- [ ] Duplicate the functionality of https://goals-viewer-api.herokuapp.com/
+- [ ] Use React to create a smooth, one-page app UX
+- [ ] Write all your code in ES2015
+- [ ] Deploy via heroku (deployment method?)
+- [ ] Add unit with Jasmine (Investigate test driven development and have at least one passing test)
+- [ ] write more concrete user stories/ optimize UX
+  - [ ] Add another view, such as goals-as-cards and/or a nicely detailed show page for goals
+  - [ ] Impliment review system?
+  - [ ] Registered users can upvote or downvote goals
+  - [ ] Registered & anonymous users can review goals (see all pages)
+  - [ ] You can search/filter the full description, potentially in table view
+
+## REQUIRED
+
+- [ ] The artifact produced is properly licensed, preferably with the [MIT license][mit-license].
+---
+
+<!-- LICENSE -->
+
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a>
+<br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
+
+[mit-license]: https://opensource.org/licenses/MIT
---
+++ index.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const request = require('request');
+const qs = require('querystring');
+const cookieSession = require('cookie-session');
+
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+// file to use .env variable
+if (app.get('env') !== 'production'){
+  require('dotenv').config();
+}
+
+// set up sessions
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}));
+
+// views is directory for all template files
+app.set('view engine', 'ejs');
+
+// home page
+app.get('/', (req, res) => {
+
+  // access_token
+  let token = req.session.github_access_token;
+
+  console.log(`TOKEN: ${token}`);
+
+  // check if there is a token
+  if(!token){
+    res.send('<h1>Please Login</h1><a href="login">Login</a>')
+  } else {
+
+    request({
+      method: 'GET',
+      url: 'https://api.github.com/repos/GuildCrafts/web-development-js/issues',
+      headers: {
+        'user-agent': 'node.js',
+        'authorization': `Token ${token}`
+      }
+    }, (error, response) => {
+      if (error) throw error;
+
+      let goals = JSON.parse(response.body);
+
+      console.log( "These are the goals", goals);
+
+      res.json(goals);
+    })
+  }
+});
+
+app.get('/login', (req, res) => {
+  // url to get code
+  let url = 'https://github.com/login/oauth/authorize?' + qs.stringify({
+    scope: 'repo',
+    client_id: process.env.GITHUB_CLIENT_ID
+  });
+
+  // authorize user
+  res.redirect(url);
+});
+
+const tokenParams = ( code ) => {
+  const client_id = process.env.GITHUB_CLIENT_ID
+  const client_secret = process.env.GITHUB_CLIENT_SECRET
+  return qs.stringify({ code, client_id, client_secret })
+}
+
+app.get('/oauth_callback', (req, res) => {
+  // url for token
+  let url = `https://github.com/login/oauth/access_token?${tokenParams( req.query.code )}`
+
+  // use the URL to get a token
+  request({
+    method: 'POST',
+    url: url
+  }, (error, response) => {
+    if (error) throw error;
+
+    // store that token in a session
+    let accessToken = qs.parse(response.body).access_token;
+    req.session.github_access_token = accessToken;
+
+    // back to home
+    res.redirect('/');
+  })
+});
+
+app.get('/comments', (req, res) => {
+  let token = req.session.github_access_token;
+  let url = `https://api.github.com/repos/GuildCrafts/web-development-js/issues/${req.body.number}/comments`
+  // RR- is req.body.number right in the link above?
+  request({
+    method: 'GET',
+    url: url,
+    headers: {
+      'user-agent': 'node.js',
+      'authorization': `Token ${token}`
+    }
+  }, (error, response) => {
+      if (error) throw error;
+      let comments = JSON.parse(response.body);
+      console.log( "These are the comments :D", comments);
+      res.json(comments);
+    })
+});
+
+
+
+app.use(express.static(__dirname+'/views'));
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/views/pages/index.html')
+});
+
+app.listen(app.get('port'), () => console.log('Node app is running on port', app.get('port')) );
