b1cca2ae988d33935aede45b74c58a7a3ab62016
James Stewart <JameStewartJr@Jamess-MacBook-Pro.local>
2016-07-22 11:42:17 -0700

    currently working on detail page.

--- public/scripts/grid.js
+++ public/scripts/grid.js
@@ -14,19 +14,19 @@ var Main = React.createClass({
       return <Row key={goal.id} goal={goal} />
     })
   },
-  
+
   sortByNumber: function(event) {
     event.preventDefault();
     this.setState({sortBy: 'number'});
   },
 
-  
+
   sortByTitle: function(event) {
     event.preventDefault();
     this.setState({sortBy: 'title'});
   },
 
-  
+
   sortByMember: function(event) {
     event.preventDefault();
     var memberSorter = function(goal){
@@ -35,7 +35,7 @@ var Main = React.createClass({
     this.setState({sortBy: memberSorter});
   },
 //in order to sort by member name we need to get all member names into a string.
-//move complexity of member name into sortedGoals. 
+//move complexity of member name into sortedGoals.
 //move membersorter function into sortedGOals function
 
   render: function() {
@@ -53,22 +53,22 @@ var Main = React.createClass({
           <tbody>
             {this.tableRows()}
           </tbody>
-      </table> 
+      </table>
     );
   }
 });
 
 var Row = React.createClass({
-  render: function() {         
+  render: function() {
 
     return (
       //this displays data on the rows of our table
       <tr>
         <IssueNumber number={this.props.goal.number} />
         <GoalLabelsCell goal={this.props.goal} />
-        <GoalTitle title={this.props.goal.title} />
-        <MemberName displayName={this.props.goal.user.login} htmlUrl={this.props.goal.htmlUrl} />
-        
+        <GoalTitle title={this.props.goal.title}  htmlUrl={this.props.goal.htmlUrl} />
+        <MemberName displayName={this.props.goal.user.login} />
+
       </tr>
     )
   }
@@ -102,28 +102,18 @@ var GoalLabelsCell = React.createClass({
   render: function() {
     var labels = this.props.goal.labels.map( function(label){
       return <GoalLabel key={label.name} label={label}/>
-    }) 
+    })
     return <td> {labels}</td>
   }
 })
 
 var GoalLabel = function(props) {
   var className = "GoalLabel"
-  if (props.label.name === "draft") className += " GoalLabel-draft" 
+  if (props.label.name === "draft") className += " GoalLabel-draft"
   return <div key={props.label.name} className={className}> {props.label.name}</div>
 }
 
 
-/*
-
-labels
-comment number
-
-XX issue title
-xx username
-xx number
-
-*/
 
 // var ImportantButton = React.createClass({
 //   render: function() {
